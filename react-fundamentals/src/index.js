00e57515712f5367080003dbe314e274342c1c90
cpsilva <caiopirees@gmail.com>
2024-06-17 20:03:46 +0100

    creating a react project from scratch

---
+++ react-fundamentals/src/index.js
@@ -0,0 +1,7 @@
+import { createRoot } from "react-dom/client";
+
+const el = document.getElementById("root");
+const root = createRoot(el);
+const tag = <strong>Hello react!</strong>;
+
+root.render(<div>{tag}</div>);
