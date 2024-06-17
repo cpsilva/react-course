876e3c3885b1e0ee93dce8fb26737f4492db61a9
cpsilva <caiopirees@gmail.com>
2024-06-17 20:32:32 +0100

    Enhancing the first component with JSX

--- react-fundamentals/src/components/basics/first.js
+++ react-fundamentals/src/components/basics/first.js
@@ -1,4 +1,9 @@
 export default function First() {
-    return 'First component'
+  const msg = "Welcome to this application!";
+  return (
+    <div>
+      <h2>First component</h2>
+      <p>{ msg }</p>
+    </div>
+  );
 }
-
